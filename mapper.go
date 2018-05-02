670c0028fb597d649a2c0be892e2161f9fd9da69
Nick Nicholas <opoudjis@optusnet.com.au>
2018-05-02 17:03:13 +1000

    report mappings

--- mapper.go
+++ mapper.go
@@ -41,9 +41,12 @@ func main() {
 	}
 	// Item    Stage   LearningArea    Strand  Substrand       Text    Elaborations
 	classes := make([]bayesian.Class, 0)
+	curriculum_map := make(map[string]string)
 	for _, record := range curriculum {
 		classes = append(classes, bayesian.Class(record["Item"]))
+		curriculum_map[record["Item"]] = record["Text"]
 	}
+
 	classifier := bayesian.NewClassifierTfIdf(classes...)
 	for _, record := range curriculum {
 		classifier.Learn(tokenize.TextToWords(record["Text"]+". "+record["Elaborations"]), bayesian.Class(record["Item"]))
@@ -57,6 +60,13 @@ func main() {
 
 	syllabus, err := read_curriculum("./testdata/syllabus.txt")
 	// Item    Stage   LearningArea    Strand  Substrand       Outcome Content AC content
+	syllabus_map := make(map[string]string)
+	for _, record := range syllabus {
+		syllabus_map[record["Item"]] = record["Outcome"]
+	}
+
+	syllabus_outcome_alignment := make(map[string]string)
+	syllabus_content_alignment := make(map[string]string)
 	for _, record := range syllabus {
 		scores1, max1, _ := classifier.LogScores(tokenize.TextToWords(record["Outcome"]))
 		scores2, max2, _ := classifier.LogScores(tokenize.TextToWords(record["Content"]))
@@ -77,13 +87,21 @@ func main() {
 			is_max1 := ""
 			if i == max1 {
 				is_max1 = "#"
+				syllabus_outcome_alignment[record["Item"]] = string(classes[i])
 			}
 			is_max2 := ""
 			if i == max2 {
 				is_max2 = "#"
+				syllabus_content_alignment[record["Item"]] = string(classes[i])
 			}
 			fmt.Printf("%0.3f%s:%0.3f%s%s\t", scores1[i], is_max1, scores2[i], is_max2, match)
 		}
 		fmt.Println()
+	}
+
+	fmt.Printf("\n\n")
+	for k, v := range syllabus_outcome_alignment {
+		fmt.Printf("The best match for NSW Outcome %s: \"%s\" is AC CD %s: \"%s\" based on outcome text, and AC CD %s: \"%s\" based on content text\n",
+			k, syllabus_map[k], v, curriculum_map[v], syllabus_content_alignment[k], curriculum_map[syllabus_content_alignment[k]])
 	}
 }
